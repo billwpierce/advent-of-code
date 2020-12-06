8f1b9172645979d6cfd55635d5943291a8f3bb1c
Billy Pierce <wwpierce020602@gmail.com>
2020-12-05 16:47:53 -0800

    part 2 code

--- 2020/code_d1.py
+++ 2020/code_d1.py
@@ -1,5 +1,11 @@
 original_input = open('day1input.txt', 'r') 
 all_lines = original_input.readlines() 
 
-for line in all_lines:
-    if 
+print(all_lines)
+
+for line1 in all_lines:
+    val1 = int(line1)
+    for line2 in all_lines:
+        val2 = int(line2)
+        if (str(2020-val1-val2) + "\n") in all_lines:
+            print(val1 * val2 * (2020-val1-val2))
