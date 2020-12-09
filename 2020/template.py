71654460bba95c309e037780777210ff1d976b53
Billy Pierce <wwpierce020602@gmail.com>
2020-12-08 22:13:20 -0800

    fixed slight template error

--- 2020/template.py
+++ 2020/template.py
@@ -2,5 +2,5 @@ f = open('day_input.txt', 'r') # Insert the correct day
 # Use one of these, depending on the day's challenges
 input = f.read().splitlines()  # Raw Reading
 # input = f.read().split("\n\n") # Groups Across Multiple Lines
-# input = [int(x) for x in inp] # Lines of integers
-# input = [x.split() for x in inp] # Multi-part input lines, seperated by spaces
+# input = [int(x) for x in input] # Lines of integers
+# input = [x.split() for x in input] # Multi-part input lines, seperated by spaces
