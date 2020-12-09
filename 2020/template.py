9764333271ad6d71567ebe847b99d61c473e28d2
Billy Pierce <wwpierce020602@gmail.com>
2020-12-08 21:40:31 -0800

    added readme

--- 2020/template.py
+++ 2020/template.py
@@ -1,6 +1,6 @@
 f = open('day_input.txt', 'r') # Insert the correct day
 # Use one of these, depending on the day's challenges
-inp = f.read().splitlines()  # Raw Reading
-# inp = f.read().split("\n\n") # Groups Across Multiple Lines
-# inp = [int(x) for x in inp] # Lines of integers
-# inp = [x.split() for x in inp] # Multi-part input lines, seperated by spaces
+input = f.read().splitlines()  # Raw Reading
+# input = f.read().split("\n\n") # Groups Across Multiple Lines
+# input = [int(x) for x in inp] # Lines of integers
+# input = [x.split() for x in inp] # Multi-part input lines, seperated by spaces
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Some Thoughts on Advent
+
+## Citations
+
+Not all of my code in advent is properly cited, as I'm trying to write the code quickly.
